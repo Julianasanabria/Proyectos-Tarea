9a09559b70206b84b615be974f7823be325d4440
santiagoCQ04 <santiagocarvajalquiroga@gmail.com>
2025-07-31 14:55:02 -0500

    cambio e los estados carpeta de rutas

--- routes/states.js
+++ routes/states.js
@@ -1,22 +1,23 @@
 import Routes from "express"
-import AuthController from "../controllers/states.js"
+import StateController from "../controllers/states.js"
+import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router = Routes()
 
 // Obtener todos los estados
-router.get('/', AuthController.verifyToken, StateController.getAllStates);
+router.get('/', validarJWT, StateController.getAllStates);
 
 // Obtener un estado por ID
-router.get('/:id', AuthController.verifyToken, StateController.getStateById);
+router.get('/:id', validarJWT, StateController.getStateById);
 
 // Crear un nuevo estado
-router.post('/', AuthController.verifyToken, StateController.createState);
+router.post('/', validarJWT, StateController.createState);
 
 // Actualizar un estado
-router.put('/:id', AuthController.verifyToken, StateController.updateState);
+router.put('/:id', validarJWT, StateController.updateState);
 
 // Eliminar un estado
-router.delete('/:id', AuthController.verifyToken, StateController.deleteState);
+router.delete('/:id', validarJWT, StateController.deleteState);
 
 
 export default router;
